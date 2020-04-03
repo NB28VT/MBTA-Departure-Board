8be14fa07ce08e15675bde9cd5b183719e91fd76
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2020-04-03 15:44:12 -0400

    Rip out json-api-normalizer

--- src/components/DepartureBoard.jsx
+++ src/components/DepartureBoard.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import APIClient from '../services/APIClient'
-import normalize from 'json-api-normalizer';
 
 // TODO: maybe use MobX here
 class DepartureBoardModel {
@@ -18,7 +17,6 @@ class DepartureBoardModel {
         const routes = await this.getStationRoutes()
         // Yuck - there is def a more eloquent way to do this
         const routeIDs = Object.values(routes.route).map(route => route.id).join()
-        console.log(routeIDs)
 
 
         // Time before which schedule should not be returned. To filter times after midnight use more than 24 hours. For example, min_time=24:00 will return schedule information for the next calendar day, since that service is considered part of the current service day. Additionally, min_time=00:00&max_time=02:00 will not return anything. The time format is HH:MM.
@@ -27,18 +25,13 @@ class DepartureBoardModel {
         // TODO: I don't think this is going to work before 10, hours might return 2 instead of 02, hmm
         const minTime = `${now.getHours()}:${now.getMinutes()}`
         const filters = `filter[stop]=${this.stationCode}&filter[route]=${routeIDs}&filter[min_time]=${minTime}`
-        const include = '&include=prediction,trip'
+        const include = '&include=prediction,trip,route'
         const sortAndLimit = '&sort=departure_time&page[limit]=10'
         const endpoint = 'schedules?' + filters + sortAndLimit + include
-        console.log(endpoint)
 
         const schedulesResponse = await this.apiClient.APIGet(endpoint)
-        console.log(normalize(schedulesResponse))
 
-
-        // If no prediction, assume on time
-
-        // TODO: this is where we will parse the data and format it for the table rows
+        return this.serializeDepartures(normalize(schedulesResponse))
     }
 
     async getStationRoutes() {
@@ -48,6 +41,50 @@ class DepartureBoardModel {
 
         return normalize(routesResponse)
     }
+
+
+    serializeDepartures(response) {
+        // If no prediction, assume on time
+        const schedules = Object.values(response.schedule)
+        const trips = Object.values(response.trip)
+        const routes = Object.values(response.route)
+        // const predictions = Object.values(response.prediction)
+        
+        return schedules.map((schedule => {
+
+            // Yuck
+            const route = routes[schedule.relationships.route.data.id]
+            debugger
+
+
+
+            // debugger;
+            // Not sure if destinations is accurate
+
+            const data = {
+                departureTime: schedule.attributes.departureTime,
+                destination: route.attributes.directionDestinations[0],
+            }
+
+            console.log(data)
+
+            // Carrier: Just do MBTA
+            // Departure Time: on the schedule endpoint
+            // Status: include prediciton and cross reference to determine if it's on time
+            // Destination: include route and maybe that gets us destination
+            // Track #: might be on the trip?
+            // Train #: might also be on the trip
+            // Station: south station place filter
+            // Commuter Rail: route_type = 2
+            // Upcoming: sort by min time I think 
+
+
+        }))
+
+        
+    }
+
+
 }
 
 export class DepartureBoard extends React.Component {
@@ -67,6 +104,7 @@ export class DepartureBoard extends React.Component {
 
     async componentDidMount() {
         const departures = await this.model.getCurrentDepartures()
+        console.log(departures)
         // I think this just works:
         this.setState(departures)
     }
