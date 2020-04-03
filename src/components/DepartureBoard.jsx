4f497d2dcc1ddb072cae84569335b9d2fdbb0f73
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2020-04-03 11:59:42 -0400

    Now retrieving most of the data I need

--- src/components/DepartureBoard.jsx
+++ src/components/DepartureBoard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import APIClient from '../services/APIClient'
+import normalize from 'json-api-normalizer';
 
+// TODO: maybe use MobX here
 class DepartureBoardModel {
     constructor(stationCode, routeType) {
         this.stationCode = stationCode
@@ -10,14 +12,41 @@ class DepartureBoardModel {
     }
 
     async getCurrentDepartures() {
+        // TODO: use fields option to only get the fields you want for schedules
+
+        // Feck we do need routes endpoint to get commuter raile routes, this schedule endpoint can't filter by route type boooo
         const routes = await this.getStationRoutes()
-        console.log(routes)
+        // Yuck - there is def a more eloquent way to do this
+        const routeIDs = Object.values(routes.route).map(route => route.id).join()
+        console.log(routeIDs)
+
+
+        // Time before which schedule should not be returned. To filter times after midnight use more than 24 hours. For example, min_time=24:00 will return schedule information for the next calendar day, since that service is considered part of the current service day. Additionally, min_time=00:00&max_time=02:00 will not return anything. The time format is HH:MM.
+
+        const now = new Date()
+        // TODO: I don't think this is going to work before 10, hours might return 2 instead of 02, hmm
+        const minTime = `${now.getHours()}:${now.getMinutes()}`
+        const filters = `filter[stop]=${this.stationCode}&filter[route]=${routeIDs}&filter[min_time]=${minTime}`
+        const include = '&include=prediction,trip'
+        const sortAndLimit = '&sort=departure_time&page[limit]=10'
+        const endpoint = 'schedules?' + filters + sortAndLimit + include
+        console.log(endpoint)
+
+        const schedulesResponse = await this.apiClient.APIGet(endpoint)
+        console.log(normalize(schedulesResponse))
+
+
+        // If no prediction, assume on time
+
+        // TODO: this is where we will parse the data and format it for the table rows
     }
 
     async getStationRoutes() {
-        // routes?filter[type]=2&filter[stop]=place-sstat&fields=id
+        // TODO: might be able to cache these in some way because they don't change, API supports 304 codes
         const endpoint = `routes?filter[type]=${this.routeType}&filter[stop]=${this.stationCode}&fields=id`
-        return await this.apiClient.APIGet(endpoint)
+        const routesResponse = await this.apiClient.APIGet(endpoint)
+
+        return normalize(routesResponse)
     }
 }
 
--- src/services/APIClient.js
+++ src/services/APIClient.js
@@ -1,27 +1,27 @@
 export default class APIClient {
     constructor() {
         this.baseURL = "https://api-v3.mbta.com/"
-        
-        // https://api-v3.mbta.com/routes?filter[type]=2&filter[stop]=place-sstat&fields=id
     }
 
-
     async APIGet(endpoint) {
-        // Look into using JSONAPI serializer here
         // I don't think I need async if it returns a promise anyway
         // TODO: can use headers here for streaming possibly
         return new Promise((resolve, reject) => {
             const url = this.baseURL + endpoint + `&api_key=${process.env.REACT_APP_MBTA_API_KEY}`
-            console.log(url)
             fetch(url)
                 .then(response => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText)
+                    }
                     return response.json()
-                }).catch(e => reject(e)) 
-            //     })
-            //     .then(body => {
-            //         return resp.ok ? resolve(body) : reject(resp)
-            //     }
-            // ).catch(error => reject(error))
+                }).then(body => {
+                    // Not sure about this
+                    resolve(body)
+                })
+                .catch(error => {
+                    console.log(error)
+                    reject(error)
+                })
         })
     }
 }
